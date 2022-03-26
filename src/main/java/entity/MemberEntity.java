b368e9fbedbc5667f3b788ec97507ec6a5d295f1
YunSeojin <tkaltk123@gmail.com>
2022-03-26 15:34:22 +0900

    회원 권한, 가입일, 수정일, 회원 설명 추가

--- src/main/java/entity/MemberEntity.java
+++ src/main/java/entity/MemberEntity.java
@@ -1,6 +1,9 @@
 package entity;
 
+import enums.RoleType;
+
 import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 @Table(name = "MEMBER")
@@ -12,11 +15,29 @@ public class MemberEntity {
     @Column(name = "NAME")
     private String name;
 
+    @Column(name = "AGE")
     private Integer age;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "ROLE_TYPE")
+    private RoleType roleType;
+
+    @Lob
+    @Column(name = "DESCRIPTION")
+    private String description;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "CREATED_AT")
+    private Date createdAt;
+
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "UPDATED_AT")
+    private Date updatedAt;
 
     @Override
     public String toString() {
-        return "id : " + id + ", name : " + name + ", age : " + age.toString();
+        return "id : " + id + ", name : " + name + ", age : " + age + ", roleType : " + roleType + ", description : " + description;
     }
 
     public String getId() {
@@ -41,5 +62,37 @@ public class MemberEntity {
 
     public void setAge(Integer age) {
         this.age = age;
+    }
+
+    public RoleType getRoleType() {
+        return roleType;
+    }
+
+    public void setRoleType(RoleType roleType) {
+        this.roleType = roleType;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
